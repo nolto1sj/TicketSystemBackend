7e4c501b9ec81550382f709b43ce916f43a23e79
nolto1sj <sjnolton@gmail.com>
2022-11-10 20:56:50 -0500

    Added services

---
+++ Services/ITicketService.cs
@@ -0,0 +1,14 @@
+﻿using TicketSystemBackend.Models;
+
+namespace TicketSystemBackend.Services
+{
+    public interface ITicketService
+    {
+        ICollection<Ticket> GetAll();
+        Ticket CreateTicket();
+        Ticket UpdateTicket();
+        bool CompleteTicket();
+        Ticket SeeTicketDetails();
+        Ticket BookmarkTicket();
+    }
+}
